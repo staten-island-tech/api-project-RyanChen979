1771b5c5b72e494f153f2fb244cf4e182755e2d9
Ryan Chen <ryanchen979@gmail.com>
2022-12-21 10:02:01 -0500

    sdfdsf

--- api-project/counter.js
+++ api-project/counter.js
@@ -1,9 +1,21 @@
 export function setupCounter(element) {
-  let counter = 0
+  let counter = 0;
   const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+    counter = count;
+    element.innerHTML = `count is ${counter}`;
+  };
+  element.addEventListener("click", function () {
+    setCounter(counter + 1);
+    random_bg_color();
+  });
+  setCounter(0);
+}
+export function random_bg_color() {
+  var x = Math.floor(Math.random() * 256);
+  var y = Math.floor(Math.random() * 256);
+  var z = Math.floor(Math.random() * 256);
+  var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+  console.log(bgColor);
+
+  document.body.style.background = bgColor;
 }
