ddaa8169f7bada736d49cc21c19ecf86564ce388
Ryan Chen <ryanchen979@gmail.com>
2023-01-10 10:15:30 -0500

    acho

--- api-project/javascript/main.js
+++ api-project/javascript/main.js
@@ -4,15 +4,12 @@ const APIurl = "https://api.jikan.moe/v4/anime?q=haikyuu&sfw";
 
 document.querySelector(".title").innerHTML = `
   <div>
-    <h1></h1>
+    <h1>API Search</h1>
   </div>
 `;
 
 document.querySelector(".search").innerHTML = `
   <div>
-    <a href="https://www.youtube.com/watch?v=5Q_2nrp9OTg" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
@@ -24,14 +21,5 @@ document.querySelector(".search").innerHTML = `
 
 document.querySelector(".content").innerHTML = `
   <div>
-    <a href="https://www.youtube.com/watch?v=5Q_2nrp9OTg" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the logos to learn more about noobs
-    </p>
   </div>
 `;
