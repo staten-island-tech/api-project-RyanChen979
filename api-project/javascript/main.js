75cb2271ff8e5f3d9a5a50f17724648517513bf0
Ryan Chen <ryanchen979@gmail.com>
2023-01-12 10:16:28 -0500

    12345678

--- api-project/javascript/main.js
+++ api-project/javascript/main.js
@@ -1,11 +1,16 @@
 import "../css/style.css";
 import { DOM } from "./dom";
-const APIurl = "https://api.jikan.moe/v4/anime?q=haikyuu&sfw";
+const APIurl = "https://valorant-api.com/v1/agents";
+
+async function getAPI(APIurl) {
+  const response = await fetch(APIurl);
+  console.log(response);
+}
+getAPI(APIurl);
 
 document.querySelector(".title").innerHTML = `
   <div>
     <h1>API Search</h1>
-    <br />
   </div>
 `;
 
