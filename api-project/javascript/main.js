539edffe3355d14de69c8237fe01ce56d9c12114
Ryan Chen <ryanchen979@gmail.com>
2023-01-22 18:48:14 -0500

    hm

--- api-project/javascript/main.js
+++ api-project/javascript/main.js
@@ -8,29 +8,41 @@ async function getData(APIurl) {
     const data = await response.json();
     console.log(data);
     data.data.forEach((el) => {
-      console.log(el.displayName, el.uuid);
-      delete data[7];
+      console.log(`Name: ${el.displayName}, UUID: ${el.uuid}`);
       DOM.content.insertAdjacentHTML(
         "beforeend",
         `<div class="agent">
-        <h2>${el.displayName}</h2>
-        <img class="image" src="${el.fullPortrait}" alt="character-image" />
-        <p class="description">${el.description}</p>
-        <p>Developer: ${el.developerName}</p>
-        </div>`
+         <h2 class="name">${el.displayName}</h2>
+         <img class="image" src="${el.fullPortrait}" alt="character-image" />
+         </div>`
       );
     });
+    return data;
   } catch (error) {
     console.log(error);
   }
 }
 getData(APIurl);
 
-async function searchAPI() {
-  try {
-    DOM.button.addEventListener("submit", function clearInputs() {
-      DOM.content = "";
-    });
-  } catch (error) {}
+function searchAPI() {
+  DOM.button.addEventListener("submit", function searchAgent() {
+    DOM.content.innerHTML = "";
+
+    let newAgent = DOM.searchInput.value;
+    console.log(newAgent);
+    function newCard(data) {
+      const thing = data.data.filter(data.data);
+      DOM.content.insertAdjacentHTML(
+        "beforeend",
+        `<div class="agent">
+           <h2 class="name">${newAgent.displayName}</h2>
+           <img class="image" src="${newAgent.fullPortrait}" alt="character-image" />
+           <p class="description">${newAgent.description}</p>
+           <p class="dev">Developer: ${newAgent.developerName}</p>
+           </div>`
+      );
+    }
+  });
+  searchAgent();
 }
 searchAPI();
