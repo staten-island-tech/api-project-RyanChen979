80b3c7feb300c9f3be93edfc41c4af5a23959fbb
Ryan Chen <ryanchen979@gmail.com>
2023-01-22 17:15:17 -0500

    eh

--- api-project/javascript/main.js
+++ api-project/javascript/main.js
@@ -1,14 +1,6 @@
 import "../css/style.css";
-import { DOM } from "./dom";
+import { DOM } from "./DOM";
 const APIurl = "https://valorant-api.com/v1/agents";
-
-/* function userInput() {
-  let input = DOM.searchInput;
-}
-
-DOM.button.addEventListener("submit", function clearInputs() {
-  DOMSelector.searchInput.value = "";
-}); */
 
 async function getData(APIurl) {
   try {
@@ -16,18 +8,29 @@ async function getData(APIurl) {
     const data = await response.json();
     console.log(data);
     data.data.forEach((el) => {
+      console.log(el.displayName, el.uuid);
+      delete data[7];
       DOM.content.insertAdjacentHTML(
         "beforeend",
         `<div class="agent">
         <h2>${el.displayName}</h2>
         <img class="image" src="${el.fullPortrait}" alt="character-image" />
         <p class="description">${el.description}</p>
+        <p>Developer: ${el.developerName}</p>
         </div>`
       );
-      console.log(el.displayName);
     });
   } catch (error) {
     console.log(error);
   }
 }
 getData(APIurl);
+
+async function searchAPI() {
+  try {
+    DOM.button.addEventListener("submit", function clearInputs() {
+      DOM.content = "";
+    });
+  } catch (error) {}
+}
+searchAPI();
